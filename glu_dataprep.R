9bb8a0f11b85e91cee9eed9a55fcd84ececcb571
vsteff <steffen.verena@gmail.com>
2016-06-02 08:53:09 +0200

    final prepared glu dataset

--- glu_dataprep.R
+++ glu_dataprep.R
@@ -117,12 +117,8 @@ glu <- glu[-which(is.na(glu$Ward.number)), ]  # remove "extra" wards that are no
 # Correct special character
 glu$Ward <- factor(gsub("\xd9", "ue", as.character(glu$Ward)))
 
-### Check data
-summary(glu)
-
-# Order data after PID and Time
+### Order data after PID and Time
 glu <- glu[order(glu$PID, glu$Time), ]
-
 
 ### Inpatient definition
 # Add column truncated date for inpatient definition
@@ -173,8 +169,9 @@ for (i in 1:npatients) {  # i: patient
   }
 }  # takes about 3.5 hours
 glu$Time.trunc <- NULL  # do not need it anymore
+glu$Inpatient <- factor(glu$Inpatient, levels = c(0, 1), labels = c("no", "yes"))
 
-### High variability
+### (High) variability
 # Calculate patient CV (%)
 CV <- function(mean, sd) (sd/mean)*100
 glu$cv <- rep(0, nrow(glu))
@@ -186,6 +183,63 @@ for (i in 1:npatients) {  # i: patient
 
 # Add column for high variability inpatients
 glu$high.var20 <- ifelse(glu$cv > 20, 1, 0)
-save(glu, file = "STA490/glu_clean.RData")
+
+### Diabetic patient yes/no
+# new way: per patient
+glu$diabetic <- rep(NA, nrow(glu))
+for (i in 1:npatients) {
+  if (sum(glu[as.numeric(glu$PID) == i, "Glu"] >= 11.1 |
+          glu[as.numeric(glu$PID) == i, "HbA1c.IFCC"] >= 48 |
+          glu[as.numeric(glu$PID) == i, "HbA1c.DCCT"] >= 6.5,
+          na.rm = TRUE) >=1) {
+    glu[as.numeric(glu$PID) == i, "diabetic"] <- 1
+  }
+  else {
+    glu[as.numeric(glu$PID) == i, "diabetic"] <- 0
+  }
+}
+glu$diabetic <- factor(glu$diabetic, levels = c(0, 1), labels = c("no", "yes"))
+
+### Glycemic control status
+# Define status according to glycemic control ranges
+glu$glyc.control <- ifelse(glu$Glu < 2.2,
+   yes = "severe hypoglycemia",
+   no = ifelse(glu$Glu < 4,
+               yes = "hypoglycemia",
+               no = ifelse(glu$Glu < 7.8,
+                           yes = "euglycemia",
+                           no = ifelse(glu$Glu < 10.1,
+                                       yes = "mild hyperglycemia",
+                                       no = ifelse(glu$Glu < 15,
+                                                   yes = "pronounced hyperglycemia",
+                                                   no = "severe hyperglycemia")))))
+glu$glyc.control <- factor(glu$glyc.control)
+
+### Flag public holidays or Sundays
+# Flag public holidays
+Sys.setlocale("LC_TIME", "en_US.UTF-8")
+glu$day <- factor(weekdays(glu$Time))
+public.holidays.bern2014 <- c("14-01-01",  # Neujahr
+                              "14-01-02",  # Berchtoldstag
+                              "14-04-18",  # Karfreitag
+                              "14-04-21",  # Ostermontag
+                              "14-05-29",  # Auffahrt
+                              "14-06-09",  # Pfingstmontag
+                              "14-08-01",  # Nationalfeiertag Schweiz
+                              "14-12-25",  # Weihnachten
+                              "14-12-26"   # Stephanstag
+                              )
+ind.ph <- NULL
+for (i in (1:length(public.holidays.bern2014))) {
+  ind.ph <- c(ind.ph, which(startsWith(as.character(glu$Time),
+                                   public.holidays.bern2014[i])))
+}
+glu$holiday <- ifelse(glu$day == "Sunday", TRUE, FALSE)
+glu$holiday[ind.ph] <- TRUE
+
+
+
+### After long computations: save workspace
+# save(glu, file = "STA490/glu_clean.RData")
 load("STA490/glu_clean.RData")
 summary(glu)
